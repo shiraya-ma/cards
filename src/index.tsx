5f4d1342b6f64f82508adaecff075dd5521076ea
shiraya-ma <mai.shirayama@outlook.jp>
2024-10-21 23:46:41 +0900

    :tada: エントリーポイント

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 'use strict';
 import { createRoot } from 'react-dom/client';
+import { RootLayout } from './root-layout';
 
 createRoot(document.querySelector('div#root')!).render(
-    <></>
+    <RootLayout>
+    </RootLayout>
 );
