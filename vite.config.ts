248fbb526260be38dd8cddedf36ba3fcb22569ba
shiraya-ma <mai.shirayama@outlook.jp>
2024-10-21 23:35:38 +0900

    :wrench: エイリアスの設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+'use strict';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
   plugins: [react()],
-})
+});
